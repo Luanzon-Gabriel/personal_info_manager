e023fa9384850b3e6eb2afea1901b4f6956cbef5
Gabriel <you@example.com>
2024-11-19 16:55:10 +0800

    First thing that I did is put the inputs I put 9 for the personal inofs

---
+++ personal_info_manager.py
@@ -0,0 +1,12 @@
+#Program 1
+
+print("Enter Personal Information")
+full_name = input("Full name: ")
+age = int(input("Age: "))
+address = input("Address: ")
+birthday = input("Birthday: ")
+height = input("Height: ")
+weight = input("Weight: ")
+nationality = input("Nationality: ")
+music_genre = input("Music genre: ")
+hobby = input("Hobby: ")
