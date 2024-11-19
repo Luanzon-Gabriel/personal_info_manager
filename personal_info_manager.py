2eb1d5e93f470f0ccc8e28cb8445ce3ea9026820
Gabriel <you@example.com>
2024-11-19 18:34:46 +0800

    I added a loop so that if the user wants to input again it will start again but if no then the loop breaks

--- personal_info_manager.py
+++ personal_info_manager.py
@@ -1,27 +1,33 @@
 #Program 1
 
-print("Enter Personal Information")
-full_name = input("Full name: ")
-age = int(input("Age: "))
-address = input("Address: ")
-birthday = input("Birthday: ")
-height = input("Height: ")
-weight = input("Weight: ")
-nationality = input("Nationality: ")
-music_genre = input("Music genre: ")
-hobby = input("Hobby: ")
+while True:
+    print("Enter Personal Information")
+    full_name = input("Full name: ")
+    age = int(input("Age: "))
+    address = input("Address: ")
+    birthday = input("Birthday: ")
+    height = input("Height: ")
+    weight = input("Weight: ")
+    nationality = input("Nationality: ")
+    music_genre = input("Music genre: ")
+    hobby = input("Hobby: ")
 
-info = (
-    f"Full Name: {full_name}\n"
-    f"Age: {age}\n"
-    f"Address: {address}\n"
-    f"Birthday: {birthday}\n"
-    f"Height: {height}\n"
-    f"Weight: {weight}\n"
-    f"Nationality: {nationality}\n"
-    f"Music genre: {music_genre}\n"
-    f"Hobby: {hobby}\n\n"
-)
+    info = (
+        f"Full Name: {full_name}\n"
+        f"Age: {age}\n"
+        f"Address: {address}\n"
+        f"Birthday: {birthday}\n"
+        f"Height: {height}\n"
+        f"Weight: {weight}\n"
+        f"Nationality: {nationality}\n"
+        f"Music genre: {music_genre}\n"
+        f"Hobby: {hobby}\n\n"
+    )
 
-with open("personal_info.txt", "a") as info_file:
-    info_file.write(info)
+    with open("personal_info.txt", "a") as info_file:
+        info_file.write(info)
+
+    another_input = input("Would you like to input another informations for another person? (yes/no): ")
+    if another_input == "no":
+        print("Information saved!")
+        break
