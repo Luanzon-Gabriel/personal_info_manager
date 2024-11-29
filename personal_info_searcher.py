b886057c84fa13bea59d1b3deeaf34c06cb8f2f7
Gabriel <you@example.com>
2024-11-29 20:50:29 +0800

    I added try except error handling and a while loop to begin the program so if the user wants to search for another name

--- personal_info_searcher.py
+++ personal_info_searcher.py
@@ -1,24 +1,28 @@
 #Program 2
 
-filename = "personal_info.txt"
-search_full_name = input("Enter full name to search: ")
+while True:
+    filename = "personal_info.txt"
+    search_full_name = input("Enter full name to search: ")
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
 
-with open(filename, "r") as file:
-    lines = file.readlines()
+        found = False
+        info = ""
 
-found = False
-info = ""
 
-for line in lines:
-    if f"Full Name: {search_full_name}" in line:
-        found = True
-    if found:
-        info += line
-        if line.strip() ==  "":
-            break
+        for line in lines:
+            if f"Full Name: {search_full_name}" in line:
+                found = True
+            if found:
+                info += line
+                if line.strip() ==  "":
+                    break
 
-if found:
-    print("Information found: ")
-    print(info)
-else:
-    print(f"No inforation found for {search_full_name}")
+        if found:
+            print("Information found: ")
+            print(info)
+        else:
+            print(f"No information found for {search_full_name}")
+    except FileNotFoundError:
+        print(f"The file {search_full_name} is not found")
