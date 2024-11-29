6350e8c2bf403c58679491af9dd232b12afd3bb1
Gabriel <you@example.com>
2024-11-29 20:15:15 +0800

    With the help of a bunch of youtube tutorials I have finally learned how to read text files and I applied it to this program and It finally worked

--- personal_info_searcher.py
+++ personal_info_searcher.py
@@ -3,3 +3,20 @@
 filename = "personal_info.txt"
 search_full_name = input("Enter full name to search: ")
 
+with open(filename, "r") as file:
+    lines = file.readlines()
+
+found = False
+info = ""
+
+for line in lines:
+    if f"Full Name: {search_full_name}" in line:
+        found = True
+    if found:
+        info += line
+        if line.strip() ==  "":
+            break
+
+if found:
+    print("Information found: ")
+    print(info)
